9eaccdd1c12ab1df1de77a6a799fc4ffad80c5f1
Jorbis21 <javierorbis@protonmail.com>
2024-02-19 16:54:43 +0100

    float

--- Arduino/CodigoPlaca/CodigoPlaca.ino
+++ Arduino/CodigoPlaca/CodigoPlaca.ino
@@ -14,7 +14,7 @@ BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint8_t txValue = 0;//this is the data send
+float txValue = 0.0;//this is the data send
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -124,6 +124,35 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (deviceConnected) {
+    Serial.print("one reading:\t");
+    txValue = scale.get_units();
+    Serial.print(txValue, 1);
+    pTxCharacteristic->setValue(txValue);
+    pTxCharacteristic->notify();
+    Serial.print("\t| average:\t");
+    txValue = scale.get_units(10);
+    Serial.println(txValue, 1);
+    pTxCharacteristic->setValue(txValue);
+    pTxCharacteristic->notify();
 
+    scale.power_down();			        // put the ADC in sleep mode
+    delay(5000);
+    scale.power_up();
+
+		delay(1000); // bluetooth stack will go into congestion, if too many packets are sent
+	}
+
+    // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+	// do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
 }
