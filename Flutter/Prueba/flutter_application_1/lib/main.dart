939b00e7c93435754d501667654b549f063484dc
Jorbis21 <javierorbis@gmail.com>
2024-06-23 20:57:11 +0200

    Intento de añadir BLE
    Me da problemas con la versión de android SDK, no se como solucionarlo.
    El código de ejemplo que he pillado de internet es antiguo así que puede ser por eso.

--- Flutter/Prueba/flutter_application_1/lib/main.dart
+++ Flutter/Prueba/flutter_application_1/lib/main.dart
@@ -1,125 +1,339 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:location_permissions/location_permissions.dart';
+import 'dart:io' show Platform;
+
+// This flutter app demonstrates an usage of the flutter_reactive_ble flutter plugin
+// This app works only with BLE devices which advertise with a Nordic UART Service (NUS) UUID
+Uuid _UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+Uuid _UART_RX   = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+Uuid _UART_TX   = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter_reactive_ble example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 5, 196, 5)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter_reactive_ble UART example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  MyHomePage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final flutterReactiveBle = FlutterReactiveBle();
+  List<DiscoveredDevice> _foundBleUARTDevices = [];
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late Stream<ConnectionStateUpdate> _currentConnectionStream;
+  late StreamSubscription<ConnectionStateUpdate> _connection;
+  late QualifiedCharacteristic _txCharacteristic;
+  late QualifiedCharacteristic _rxCharacteristic;
+  late Stream<List<int>> _receivedDataStream;
+  late TextEditingController _dataToSendText;
+  bool _scanning = false;
+  bool _connected = false;
+  String _logTexts = "";
+  List<String> _receivedData = [];
+  int _numberOfMessagesReceived = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    _dataToSendText = TextEditingController();
+  }
+
+  void refreshScreen() {
+    setState(() {});
+  }
+
+  void _sendData() async {
+      await flutterReactiveBle.writeCharacteristicWithResponse(_rxCharacteristic, value: _dataToSendText.text.codeUnits);
+  }
+
+  void onNewReceivedData(List<int> data) {
+    _numberOfMessagesReceived += 1;
+    _receivedData.add( "$_numberOfMessagesReceived: ${String.fromCharCodes(data)}");
+    if (_receivedData.length > 5) {
+      _receivedData.removeAt(0);
+    }
+    refreshScreen();
+  }
+
+  void _disconnect() async {
+    await _connection.cancel();
+    _connected = false;
+    refreshScreen();
+  }
+
+  void _stopScan() async {
+    await _scanStream.cancel();
+    _scanning = false;
+    refreshScreen();
+  }
+
+  Future<void> showNoPermissionDialog() async => showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) => AlertDialog(
+          title: const Text('No location permission '),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('No location permission granted.'),
+                const Text('Location permission is required for BLE to function.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Acknowledge'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+    );
+
+  void _startScan() async {
+    bool goForIt=false;
+    PermissionStatus permission;
+    if (Platform.isAndroid) {
+      permission = await LocationPermissions().requestPermissions();
+      if (permission == PermissionStatus.granted)
+        goForIt=true;
+    } else if (Platform.isIOS) {
+      goForIt=true;
+    }
+    if (goForIt) { //TODO replace True with permission == PermissionStatus.granted is for IOS test
+      _foundBleUARTDevices = [];
+      _scanning = true;
+      refreshScreen();
+      _scanStream =
+          flutterReactiveBle.scanForDevices(withServices: [_UART_UUID]).listen((
+              device) {
+            if (_foundBleUARTDevices.every((element) =>
+            element.id != device.id)) {
+              _foundBleUARTDevices.add(device);
+              refreshScreen();
+            }
+          }, onError: (Object error) {
+            _logTexts =
+                "${_logTexts}ERROR while scanning:$error \n";
+            refreshScreen();
+          }
+          );
+    }
+    else {
+      await showNoPermissionDialog();
+    }
+  }
+
+  void onConnectDevice(index) {
+    _currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice(
+      id:_foundBleUARTDevices[index].id,
+      prescanDuration: Duration(seconds: 1),
+      withServices: [_UART_UUID, _UART_RX, _UART_TX],
+    );
+    _logTexts = "";
+    refreshScreen();
+    _connection = _currentConnectionStream.listen((event) {
+      var id = event.deviceId.toString();
+      switch(event.connectionState) {
+        case DeviceConnectionState.connecting:
+          {
+            _logTexts = "${_logTexts}Connecting to $id\n";
+            break;
+          }
+        case DeviceConnectionState.connected:
+          {
+            _connected = true;
+            _logTexts = "${_logTexts}Connected to $id\n";
+            _numberOfMessagesReceived = 0;
+            _receivedData = [];
+            _txCharacteristic = QualifiedCharacteristic(serviceId: _UART_UUID, characteristicId: _UART_TX, deviceId: event.deviceId);
+            _receivedDataStream = flutterReactiveBle.subscribeToCharacteristic(_txCharacteristic);
+            _receivedDataStream.listen((data) {
+               onNewReceivedData(data);
+            }, onError: (dynamic error) {
+              _logTexts = "${_logTexts}Error:$error$id\n";
+            });
+            _rxCharacteristic = QualifiedCharacteristic(serviceId: _UART_UUID, characteristicId: _UART_RX, deviceId: event.deviceId);
+            break;
+          }
+        case DeviceConnectionState.disconnecting:
+          {
+            _connected = false;
+            _logTexts = "${_logTexts}Disconnecting from $id\n";
+            break;
+          }
+        case DeviceConnectionState.disconnected:
+          {
+            _logTexts = "${_logTexts}Disconnected from $id\n";
+            break;
+          }
+      }
+      refreshScreen();
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Text("BLE UART Devices found:"),
+            Container(
+                margin: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.blue,
+                  width:2
+                )
+              ),
+              height: 100,
+              child: ListView.builder(
+                  itemCount: _foundBleUARTDevices.length,
+                  itemBuilder: (context, index) => Card(
+                        child: ListTile(
+                          dense: true,
+                          enabled: !((!_connected && _scanning) || (!_scanning && _connected)),
+                          trailing: GestureDetector(
+                            behavior: HitTestBehavior.translucent,
+                            onTap: () {
+                              (!_connected && _scanning) || (!_scanning && _connected)? (){}: onConnectDevice(index);
+                            },
+                            child: Container(
+                              width: 48,
+                              height: 48,
+                              padding: const EdgeInsets.symmetric(vertical: 4.0),
+                              alignment: Alignment.center,
+                              child: const Icon(Icons.add_link),
+                            ),
+                          ),
+                          subtitle: Text(_foundBleUARTDevices[index].id),
+                          title: Text("$index: ${_foundBleUARTDevices[index].name}"),
+                    ))
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Text("Status messages:"),
+            Container(
+                margin: const EdgeInsets.all(3.0),
+               width:1400,
+               decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(10),
+               border: Border.all(
+                  color: Colors.blue,
+                  width:2
+                  )
+               ),
+               height: 90,
+               child: Scrollbar(
+
+                   child: SingleChildScrollView(
+                      child: Text(_logTexts)
+               )
+               )
             ),
-          ],
+            const Text("Received data:"),
+            Container(
+                margin: const EdgeInsets.all(3.0),
+                width:1400,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                        color: Colors.blue,
+                        width:2
+                    )
+                ),
+                height: 90,
+                child: Text(_receivedData.join("\n"))
+            ),
+            const Text("Send message:"),
+            Container(
+                margin: const EdgeInsets.all(3.0),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                        color: Colors.blue,
+                        width:2
+                    )
+                ),
+                child: Row(
+                    children: <Widget> [
+                      Expanded(
+                          child: TextField(
+                            enabled: _connected,
+                            controller: _dataToSendText,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter a string'
+                          ),
+                        )
+                      ),
+                      ElevatedButton(
+                          child: Icon(
+                            Icons.send,
+                            color:_connected ? Colors.blue : Colors.grey,
+                          ),
+                          onPressed: _connected ? _sendData: (){}
+                      ),
+                    ]
+            ))
+           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      persistentFooterButtons: [
+        Container(
+          height: 35,
+          child: Column(
+            children: [
+              if (_scanning) const Text("Scanning: Scanning") else const Text("Scanning: Idle"),
+              if (_connected) const Text("Connected") else const Text("disconnected."),
+            ],
+          ) ,
+        ),
+        ElevatedButton(
+          onPressed: !_scanning && !_connected ? _startScan : (){},
+          child: Icon(
+            Icons.play_arrow,
+            color: !_scanning && !_connected ? Colors.blue: Colors.grey,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: _scanning ? _stopScan: (){},
+          child: Icon(
+            Icons.stop,
+            color:_scanning ? Colors.blue: Colors.grey,
+          )
+        ),
+        ElevatedButton(
+            onPressed: _connected ? _disconnect: (){},
+            child: Icon(
+              Icons.cancel,
+              color:_connected ? Colors.blue:Colors.grey,
+        )
+        )
+      ],
     );
-  }
 }
