4b58741fec6540f49e7aae66568188e0a2ec7092
Nachoski08 <nachosantatecla@gmail.com>
2024-07-11 22:44:25 +0200

    Comunicación uart desde placa a aplicación

--- Flutter/MedPotApp/medpotapp/lib/logic/bleController.dart
+++ Flutter/MedPotApp/medpotapp/lib/logic/bleController.dart
@@ -1,19 +1,21 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:medpotapp/pages/home.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
 //metet paquete de permisos y modificar los archivos correspondientes en la carpeta android
 //Estan en los mensajes de disc
 Uuid UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
-Uuid UART_RX   = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
-Uuid UART_TX   = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+Uuid UART_RX = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+Uuid UART_TX = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 
 String targetDevice = 'Medidor de Potencia';
 
-class BLEController{
-  static final BLEController _singleton =
-      BLEController._internalConstructor();
+class BLEController {
+  static final BLEController _singleton = BLEController._internalConstructor();
 
   factory BLEController() {
     return _singleton;
@@ -30,33 +32,35 @@ class BLEController{
   QualifiedCharacteristic? txCharacteristic;
   QualifiedCharacteristic? rxCharacteristic;
   Stream<List<int>>? receivedDataStream;
+  HomePageState? ui;
   bool scanning = false;
   bool connected = false;
   /* A partir de aqui funciones de recepcion y envio de datos */
   /* Permisos */
-  Future<void> showNoPermissionDialog(BuildContext context) async => showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) => AlertDialog(
-      title: const Text('No location permission '),
-      content: const SingleChildScrollView(
-        child: ListBody(
-          children: <Widget>[
-            Text('No location permission granted.'),
-            Text('Location permission is required for BLE to function.'),
+  Future<void> showNoPermissionDialog(BuildContext context) async =>
+      showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('No location permission '),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('No location permission granted.'),
+                Text('Location permission is required for BLE to function.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Acknowledge'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ],
         ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          child: const Text('Acknowledge'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
-    ),
-  );
+      );
   Future<bool> checkAndroidBLEPermissions() async {
     if (Platform.isAndroid) {
       bool isLocation = true,
@@ -91,78 +95,86 @@ class BLEController{
     }
     return Future.value(true);
   }
+
   void stopScanAndConnect(DiscoveredDevice device) async {
     await stopScan();
     onConnect(device);
   }
 
-  Future<void> stopScan() async{
+  Future<void> stopScan() async {
     await scanStream!.cancel();
     scanning = false;
   }
-  void onConnect(DiscoveredDevice device){
-    flutterReactiveBle.requestMtu(deviceId: device.id, mtu: 64);//seguro que hay que cambiar el mtu
+
+  void onConnect(DiscoveredDevice device) {
+    flutterReactiveBle.requestMtu(
+        deviceId: device.id, mtu: 64); //seguro que hay que cambiar el mtu
     currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice(
-      id: device.id, 
-      withServices: [UART_UUID, UART_RX, UART_TX], 
-      prescanDuration: const Duration(seconds: 1)
-    );
-    connection = currentConnectionStream!.listen((event){
+        id: device.id,
+        withServices: [UART_UUID, UART_RX, UART_TX],
+        prescanDuration: const Duration(seconds: 1));
+    connection = currentConnectionStream!.listen((event) {
       var id = event.deviceId.toString();
-      switch(event.connectionState){
+      switch (event.connectionState) {
         case DeviceConnectionState.connecting:
-        {
-          break;
-        }
+          {
+            break;
+          }
         case DeviceConnectionState.connected:
-        {
-          connected = true;
-          txCharacteristic = QualifiedCharacteristic(
-            characteristicId: UART_TX, 
-            serviceId: UART_UUID, 
-            deviceId: event.deviceId
-          );
-          receivedDataStream = flutterReactiveBle.subscribeToCharacteristic(txCharacteristic!);
-          receivedDataStream!.listen((data){
-            /* Llamada a la funcion de manejo de datos */
-          });/* Falta control de errores */
-          rxCharacteristic = QualifiedCharacteristic(
-            characteristicId: UART_RX, 
-            serviceId: UART_UUID, 
-            deviceId: event.deviceId
-          );
-          break;
-        }
+          {
+            connected = true;
+            txCharacteristic = QualifiedCharacteristic(
+                characteristicId: UART_TX,
+                serviceId: UART_UUID,
+                deviceId: event.deviceId);
+            receivedDataStream =
+                flutterReactiveBle.subscribeToCharacteristic(txCharacteristic!);
+            receivedDataStream!.listen((data) {
+              if (ui == null) {
+                print("UI is null");
+                return;
+              }
+              //print(utf8.decode(data));
+              ui!.updateData(utf8.decode(data));
+            }); /* Falta control de errores */
+            rxCharacteristic = QualifiedCharacteristic(
+                characteristicId: UART_RX,
+                serviceId: UART_UUID,
+                deviceId: event.deviceId);
+            break;
+          }
         case DeviceConnectionState.disconnecting:
-        {
-          connected = false;
-          break;
-        }
+          {
+            connected = false;
+            break;
+          }
         case DeviceConnectionState.disconnected:
-        {
-          break;
-        }
+          {
+            break;
+          }
       }
     });
   }
-  void startScan() async{
+
+  void startScan() async {
     bool goForIt = await checkAndroidBLEPermissions();
-    if(goForIt){
+    if (goForIt) {
       scanning = true;
-      scanStream = flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
-        if(device.name == targetDevice){
+      scanStream =
+          flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
+        if (device.name == targetDevice) {
           stopScanAndConnect(device);
         }
       });
       /* Faltan control de errores*/
     }
-      
   }
+
   void disconnect() async {
     await connection!.cancel();
     connected = false;
   }
-  
+
   void sendDataRaw(int? value) async {
     if (value == null) return;
     if (connected) {
--- Flutter/MedPotApp/medpotapp/lib/pages/home.dart
+++ Flutter/MedPotApp/medpotapp/lib/pages/home.dart
@@ -2,66 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:medpotapp/logic/bleController.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title;
   @override
   HomePageState createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   bool connect = false;
   String iconPath = 'assets/icons/bluetooth-slash.svg';
   List<String> rxData = [];
   List<String> txData = [];
-  BLEController ble = BLEController();
+  late BLEController ble;
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    ble = BLEController();
+    ble.ui = this;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 67, 67, 67),
-      appBar: _appBar(),
-      body: ListView(
-        children: [
-          const SizedBox(height: 40,),
-          _recieveData()
-          
-        ],
-      )
-    );
+        backgroundColor: const Color.fromARGB(255, 67, 67, 67),
+        appBar: _appBar(),
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            _recieveData()
+          ],
+        ));
   }
 
   Column _recieveData() {
     return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
           ' Recieve Data',
           style: TextStyle(
-            color: Color.fromARGB(255, 210, 210, 210),
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),
+              color: Color.fromARGB(255, 210, 210, 210),
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
         ),
         Container(
-          margin: const EdgeInsets.all(3.0),
-          width:1400,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: const Color.fromARGB(255, 0, 0, 0),
-              width:2
-            )
-          ),
-          height: 90,
-          child: Text(
-            rxData.join("\n"),
-            style: const TextStyle(
-              color: Color.fromARGB(255, 210, 210, 210),
-            ),
-          )
-        )
-          ],
-        );
+            margin: const EdgeInsets.all(3.0),
+            width: 1400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                    color: const Color.fromARGB(255, 0, 0, 0), width: 2)),
+            height: 90,
+            child: Text(
+              rxData.join("\n"),
+              style: const TextStyle(
+                color: Color.fromARGB(255, 210, 210, 210),
+              ),
+            ))
+      ],
+    );
   }
 
   AppBar _appBar() {
@@ -72,10 +76,9 @@ class HomePageState extends State<HomePage>{
       title: const Text(
         'Medidor de Potencia',
         style: TextStyle(
-          color: Color.fromARGB(255, 210, 210, 210),
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        ),
+            color: Color.fromARGB(255, 210, 210, 210),
+            fontSize: 18,
+            fontWeight: FontWeight.bold),
       ),
       actions: [
         Container(
@@ -88,29 +91,39 @@ class HomePageState extends State<HomePage>{
           ),
           alignment: Alignment.center,
           child: IconButton(
-            onPressed: (){
-              if(!ble.connected){
-                if(!ble.scanning){
-                  ble.startScan();
-                  iconPath = 'assets/icons/bluetooth-signal.svg';
-                }
-                else{
-                  ble.stopScan();
+              onPressed: () {
+                if (!ble.connected) {
+                  if (!ble.scanning) {
+                    ble.startScan();
+                    iconPath = 'assets/icons/bluetooth-signal.svg';
+                    setState(() {});
+                  } else {
+                    ble.stopScan();
+                    iconPath = 'assets/icons/bluetooth-slash.svg';
+                    setState(() {});
+                  }
+                } else {
+                  ble.disconnect();
                   iconPath = 'assets/icons/bluetooth-slash.svg';
+                  setState(() {});
                 }
-              }
-              else{
-                ble.disconnect();
-                iconPath = 'assets/icons/bluetooth-slash.svg';
-              }
-              setState(() {});
-            },
-            icon: SvgPicture.asset(
-                ble.connected ? 'assets/icons/bluetooth-on.svg' : iconPath
-              )
-            ),
+                setState(() {});
+              },
+              icon: SvgPicture.asset(
+                  ble.connected ? 'assets/icons/bluetooth-on.svg' : iconPath)),
         ),
       ],
     );
+  }
+
+  void updateData(String data) {
+    count++;
+    if (count > 10) {
+      rxData.clear();
+      rxData.add(data);
+      count = 0;
+      setState(() {});
+    }
+    print(data);
   }
 }
